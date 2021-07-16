c8c68a226cce8c8e3ed6a420fd89a0abd9b33681
Charan NK <charkeet@gmail.com>
2021-07-16 23:27:56 +0530

    jupyter notebook with database

--- query3.sql
+++ query3.sql
@@ -4,4 +4,4 @@
 SELECT DISTINCT p.name from PERSON p
 INNER JOIN M_CAST mc ON mc.pid = p.pid
 INNER JOIN MOVIE mv ON mv.mid = mc.mid
-where mv.year < 1970 and mv.year > 1990;
+where CAST(SUBSTR(mv.year,-4) AS Integer)>1990 and CAST(SUBSTR(mv.year,-4) AS Integer)>1990;
